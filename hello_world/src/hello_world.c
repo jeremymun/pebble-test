113667b2cfa9276f00285ce7d1df894ba855615a
Jeremy Mun <jeremy.mwh@gmail.com>
2014-04-19 14:02:30 +0800

    a new layer has been added

--- hello_world/src/hello_world.c
+++ hello_world/src/hello_world.c
@@ -1,8 +1,8 @@
 #include <pebble.h>
 
-static Window *window;
+static Window *door;
 static TextLayer *text_layer;
-
+static TextLayer *jm_layer;
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
 }
@@ -29,6 +29,11 @@ static void window_load(Window *window) {
   text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+  jm_layer = text_layer_create((GRect) { .origin = { 0, 36 }, .size = { bounds.size.w, 30 } });
+  text_layer_set_text(jm_layer, "Hello Jeremy");
+  text_layer_set_text_alignment(jm_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(jm_layer));
 }
 
 static void window_unload(Window *window) {
@@ -36,24 +41,24 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
+  door = window_create();
+  window_set_click_config_provider(door, click_config_provider);
+  window_set_window_handlers(door, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
   const bool animated = true;
-  window_stack_push(window, animated);
+  window_stack_push(door, animated);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+  window_destroy(door);
 }
 
 int main(void) {
   init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", door);
 
   app_event_loop();
   deinit();
